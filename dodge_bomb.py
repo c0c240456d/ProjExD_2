a9b31dbeb2c670f8b235a81ae49c1ab5f460345a
c0c240456d <c0c240456d@edu.teu.ac.jp>
2025-09-30 14:15:26 +0900

    第一問第二問

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,9 +1,16 @@
 import os
+import random
 import sys
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
+DELTA={
+    pg.K_UP:(0,-5),
+    pg.K_DOWN:(0,+5),
+    pg.K_LEFT:(-5,0),
+    pg.K_RIGHT:(+5,0),
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -14,6 +21,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img=pg.Surface((20,30))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_img.set_colorkey((0,0,0))
+    bb_rct=bb_img.get_rect()
+    bb_rct.centerx=random.randint(0,WIDTH)
+    bb_rct.centery=random.randint(0,HEIGHT)
+    vx,vy=+5,+5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -24,16 +38,22 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        for key,mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        #if key_lst[pg.K_UP]:
+         #   sum_mv[1] -= 5
+        #if key_lst[pg.K_DOWN]:
+        #    sum_mv[1] += 5
+        #if key_lst[pg.K_LEFT]:
+        #    sum_mv[0] -= 5
+        #if key_lst[pg.K_RIGHT]:
+        #    sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx,vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
