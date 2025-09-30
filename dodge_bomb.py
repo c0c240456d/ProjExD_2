a597675a36798af670709e748a5c0431e4a0d9d6
c0c240456d <c0c240456d@edu.teu.ac.jp>
2025-09-30 14:42:40 +0900

    第4問

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -44,6 +44,8 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        if kk_rct.colliderect(bb_rct):
+            return
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
